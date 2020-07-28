c57f73e1ae73517d70b40a90a82abae6702ebf82
Alexander Mischuk <mischuk.alexander@gmail.com>
2020-07-28 11:40:19 +0300

    feat: upd react version

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 #!/usr/bin/env node
+const modules = "./node_modules/am-cli";
 const execa = require("execa");
 const Listr = require("listr");
 const inquirer = require("inquirer");
+const fse = require("fs-extra");
+const fs = require("fs");
+let dependencies = [
+  "stylelint",
+  "react-transition-group",
+  "node-sass",
+  "prop-types",
+  "ress",
+  "react-router-dom",
+  "prettier",
+  "stylelint-prettier",
+  "stylelint-config-prettier",
+  "stylelint-order",
+  "stylelint-scss",
+  "stylelint-no-unsupported-browser-features",
+];
+const copyFolders = (froma, toa) => {
+  fse.copy(froma, toa, function (err) {
+    if (err) return console.error(err);
+  });
+};
+
+const copyFiles = (froma, toa) => {
+  fs.copyFile(froma, toa, function (err) {
+    if (err) return console.error(err);
+  });
+};
+
 const runReactApp = () => {
   new Listr([
     {
-      title: "Creating react app...",
+      title: "Install create-react-app",
       task: () => execa("npx create-react-app ."),
+    },
+    {
+      title: "Add linter rules",
+      task: () => {
+        copyFiles(`${modules}/source/common/.prettierrc`, "./.prettierrc");
+        copyFiles(
+          `${modules}/source/common/.stylelintignore`,
+          "./.stylelintignore",
+        );
+        copyFiles(`${modules}/source/common/.stylelintrc`, "./.stylelintrc");
+      },
+    },
+    {
+      title: "Add styles",
+      task: () => {
+        copyFolders(`${modules}/source/common/styles`, "./src/styles");
+      },
+    },
+    {
+      title: "Running npm install",
+      task: () => execa("npm", ["install", ...dependencies]),
     },
   ]).run();
 };
